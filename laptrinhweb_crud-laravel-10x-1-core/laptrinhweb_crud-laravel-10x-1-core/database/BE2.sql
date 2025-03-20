a04d23e14f0596a59a574ebe88bd966f026a0a9d
LVanToan <toanl7288@gmail.com>
2025-03-20 19:07:32 +0700

    exe2

---
+++ laptrinhweb_crud-laravel-10x-1-core/laptrinhweb_crud-laravel-10x-1-core/database/BE2.sql
@@ -0,0 +1,50 @@
+﻿CREATE DATABASE BE2
+GO
+USE BE2
+go
+
+-- Bảng users
+CREATE TABLE users (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_name VARCHAR(25) NOT NULL,
+    user_email VARCHAR(55) NOT NULL,
+    user_pass VARCHAR(255) NOT NULL,
+    updated_at DATETIME DEFAULT GETDATE(),
+    created_at DATETIME DEFAULT GETDATE()
+);
+GO
+
+-- Bảng products
+CREATE TABLE products (
+    product_id INT IDENTITY(1,1) PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    product_price FLOAT NOT NULL,
+    product_description TEXT NOT NULL,
+    updated_at DATETIME DEFAULT GETDATE(),
+    created_at DATETIME DEFAULT GETDATE()
+);
+GO
+
+-- Bảng orders
+CREATE TABLE orders (
+    order_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT NOT NULL,
+    updated_at DATETIME DEFAULT GETDATE(),
+    created_at DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+GO
+
+-- Bảng order_details
+CREATE TABLE order_details (
+    order_detail_id INT IDENTITY(1,1) PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    updated_at DATETIME DEFAULT GETDATE(),
+    created_at DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+);
+GO
+
+
